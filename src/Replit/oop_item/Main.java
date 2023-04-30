8fad66271de8b5c33374b4675a727e727d8ce920
goce <goce.trpevski@gmail.com>
2023-04-29 21:28:47 -0400

    added practice description for oop-Item

--- src/Replit/oop_item/Main.java
+++ src/Replit/oop_item/Main.java
@@ -17,3 +17,26 @@ public class Main extends Item{
     }
 
 }
+/*
+### Create the `Item.java` file according the following requirements. The `Main` class will use the Item class objects to test different scenarios.
+- name (String)
+    - price (double)
+
+  - create this `constructor` to initialize the variables:
+    - 2 argument: name, price
+
+  - create a `toString()` method  to disply the object in this format:
+    > $itemName - $itemPrice
+
+#### Main topics: class & object, instance variables, constructors, toString
+
+Example:
+```
+  Item item = new Item("Folder", 4.99);
+  System.out.println(item);
+  Output:
+    Folder - $4.99
+  - declare these `instance variables`:
+
+```
+ */

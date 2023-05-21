95b9d605f8e2c5215475cb5b37ae46618d61b4da
Goce_Cydeo <goce.trpevski@gmail.com>
2023-05-20 20:26:14 -0400

    added StringSplosion

---
+++ src/CodingBat/warmup2/StringSplosion.java
@@ -0,0 +1,23 @@
+package CodingBat.warmup2;
+
+public class StringSplosion {
+
+    public String stringSplosion(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            result += str.substring(0, i+1);
+
+        }
+        return result;
+    }
+
+}
+/*
+
+Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+
+stringSplosion("Code") → "CCoCodCode"
+stringSplosion("abc") → "aababc"
+stringSplosion("ab") → "aab"
+ */

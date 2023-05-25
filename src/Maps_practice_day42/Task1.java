1aa2db059febaeb498eaab3840a006e65f15748c
Goce_Cydeo <goce.trpevski@gmail.com>
2023-05-25 19:08:25 -0400

    Started Task1 file.

---
+++ src/Maps_practice_day42/Task1.java
@@ -0,0 +1,17 @@
+package Maps_practice_day42;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
+
+/*
+1. Create a map that can contain the student name (String) and student scores (int[]) as a pair (Assume that each student has 5 scores and make the student name unique)
+		1.1 add at least 5 pairs into the map
+		1.2 Write a program that can display each student name and scores
+ */

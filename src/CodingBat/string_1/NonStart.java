a07a9efeaf647755c66057c3b391cdf73d682c7e
Goce_Cydeo <goce.trpevski@gmail.com>
2023-05-29 13:42:49 -0400

    NonStart

---
+++ src/CodingBat/string_1/NonStart.java
@@ -0,0 +1,19 @@
+package CodingBat.string_1;
+
+public class NonStart {
+    public static void main(String[] args) {
+
+    }
+
+    public String nonStart(String a, String b) {
+         return a.substring(1)+b.substring(1);
+    }
+
+}
+/*
+Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+
+nonStart("Hello", "There") → "ellohere"
+nonStart("java", "code") → "avaode"
+nonStart("shotl", "java") → "hotlava"
+ */

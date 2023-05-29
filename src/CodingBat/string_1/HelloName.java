8403de77f26926a9b127067c5b9f886f778621f2
Goce_Cydeo <goce.trpevski@gmail.com>
2023-05-29 10:21:53 -0400

    Finnished HelloName task

---
+++ src/CodingBat/string_1/HelloName.java
@@ -0,0 +1,20 @@
+package CodingBat.string_1;
+
+public class HelloName {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+
+}
+/*
+Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+helloName("Bob") → "Hello Bob!"
+helloName("Alice") → "Hello Alice!"
+helloName("X") → "Hello X!"
+ */

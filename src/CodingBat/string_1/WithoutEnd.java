14400289ef1c3361b861c9a5f3337cd0e03d0600
Goce_Cydeo <goce.trpevski@gmail.com>
2023-05-29 13:24:26 -0400

    Finnished WithoutEnd task

---
+++ src/CodingBat/string_1/WithoutEnd.java
@@ -0,0 +1,19 @@
+package CodingBat.string_1;
+
+public class WithoutEnd {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String withoutEnd(String str) {
+        return str.substring(1, str.length()-1);
+    }
+}
+/*
+Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+
+withoutEnd("Hello") → "ell"
+withoutEnd("java") → "av"
+withoutEnd("coding") → "odin"
+ */

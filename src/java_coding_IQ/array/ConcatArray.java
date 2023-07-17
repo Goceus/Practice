3024ebc6c3bc4cebc25464481c05ee797d90014d
Goce_Cydeo <goce.trpevski@gmail.com>
2023-07-17 12:14:27 -0400

    Concat Arrays task is finished.

--- src/java_coding_IQ/array/ConcatArray.java
+++ src/java_coding_IQ/array/ConcatArray.java
@@ -1,5 +1,7 @@
 package java_coding_IQ.array;
 
+import java.util.Arrays;
+
 public class ConcatArray {
 
     /*
@@ -14,14 +16,24 @@ public class ConcatArray {
                 [1, 4, 5, 8, 12, 5, 9]
      */
 
-public static int[] concatArrays(){
+public static int[] concatArrays(int[] arr1, int[]arr2){
 
 
+    int[] merge = Arrays.copyOf(arr1, arr1.length+arr2.length);
+
+    for (int i = arr1.length; i < merge.length ; i++) {
+        for (int j = 0; j < arr2.length; j++) {
+                merge[i++] = arr2[j];
+        }
+    }
+        return merge;
 }
 
     public static void main(String[] args) {
 
-
+    int[] a = {1, 4, 5};
+    int[] b = {8, 12, 5, 9};
+        System.out.println(Arrays.toString(concatArrays(a, b)));
 
     }
 }

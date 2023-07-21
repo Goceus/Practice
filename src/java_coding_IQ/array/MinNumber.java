acf14f0f4225ad7fbdf44a7aaf7f2c40bdc95873
Goce_Cydeo <goce.trpevski@gmail.com>
2023-07-21 19:34:04 -0400

    MinNumber task is finished.

--- src/java_coding_IQ/array/MinNumber.java
+++ src/java_coding_IQ/array/MinNumber.java
@@ -19,4 +19,22 @@ public class MinNumber {
      */
 
 
+    public static int minNumber(int[] arr){
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+
+        int[] x = {3, 5, 1, 5, -1, -5};
+        System.out.println("The smallest number in the array is: " + minNumber(x));
+
+    }
+
+
 }

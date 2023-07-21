4041269e097d2a22773089f6bd22c54a4198e0a2
Goce_Cydeo <goce.trpevski@gmail.com>
2023-07-21 19:54:56 -0400

    MoveZerosToEnd task is finished.

--- src/java_coding_IQ/array/MoveZerosToEnd.java
+++ src/java_coding_IQ/array/MoveZerosToEnd.java
@@ -1,5 +1,7 @@
 package java_coding_IQ.array;
 
+import java.util.Arrays;
+
 public class MoveZerosToEnd {
      /*
         Move Zeros to End
@@ -13,6 +15,25 @@ public class MoveZerosToEnd {
                [5, 2, 1, 23, 0, 0, 0, 0]
      */
 
+    public static int[] moveZerosToEnd(int[] arr) {
 
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if(arr[i] == 0){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+        int[] x = {5, 0, 2, 0, 7980, 34, 20, 0, 1, 0, 23, 999, 190, 0, 33};
+
+        System.out.println("moveZerosToEnd(x) = " + Arrays.toString(moveZerosToEnd(x)));
+    }
 
 }

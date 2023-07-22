471d3014e6b2d0d5fcbe34046deb2d13fb955026
Goce_Cydeo <goce.trpevski@gmail.com>
2023-07-21 20:48:52 -0400

    ReverseArray task is finished.

--- src/java_coding_IQ/array/ReverseArray.java
+++ src/java_coding_IQ/array/ReverseArray.java
@@ -1,5 +1,7 @@
 package java_coding_IQ.array;
 
+import java.util.Arrays;
+
 public class ReverseArray {
 
     /*
@@ -15,9 +17,22 @@ public class ReverseArray {
 
      */
 
+    public static int[] reverseArr(int[] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        return arr;
+    }
+
 
     public static void main(String[] args) {
 
+        int[] x = {3, 4, 5, 1, 2, 6};
+        System.out.println(Arrays.toString(reverseArr(x)));
 
     }
 }

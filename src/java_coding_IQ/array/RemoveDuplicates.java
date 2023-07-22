c5a627b50f932f07dd28fdc46ce5b722bed90fd9
Goce_Cydeo <goce.trpevski@gmail.com>
2023-07-21 20:35:57 -0400

    RemoveDuplicates task is finished.

--- src/java_coding_IQ/array/RemoveDuplicates.java
+++ src/java_coding_IQ/array/RemoveDuplicates.java
@@ -1,5 +1,7 @@
 package java_coding_IQ.array;
 
+import java.util.*;
+
 public class RemoveDuplicates {
 
      /*
@@ -16,5 +18,27 @@ public class RemoveDuplicates {
 
      */
 
+    public static int[] removeDuplicate(int[] arr){
+
+        Set<Integer> unique = new LinkedHashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            unique.add(arr[i]);
+        }
+        int[] uniqueArr = new int[unique.size()];
+        int index = 0;
+
+        for (Integer each : unique){
+            uniqueArr[index++]=each;
+        }
+        return uniqueArr;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] x = {3, 3, 4, 5, 1, 2, 6, 2, 5, 4, 2, 1, 3};
+        System.out.println(Arrays.toString(removeDuplicate(x)));
+
+    }
 
 }

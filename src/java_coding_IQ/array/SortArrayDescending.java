dbb58609f10bdf97410c27a240f96355e3a90bf3
Goce_Cydeo <goce.trpevski@gmail.com>
2023-08-01 11:43:46 -0400

    SortArrayDescending file is created.

---
+++ src/java_coding_IQ/array/SortArrayDescending.java
@@ -0,0 +1,27 @@
+package java_coding_IQ.array;
+
+public class SortArrayDescending {
+
+     /*
+        Sort Array Descending
+
+        Create a method that will sort the given array in descending order (largest to smallest).
+
+        DO NOT USE any built in methods
+
+        Ex:
+            Input:
+                [3, 4, 1, 3, 8, 6, -1, 5, 1, 2, 6]
+            Output:
+                [8, 6, 6, 5, 4, 3, 3, 2, 1, 1, -1]
+
+     */
+
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+}

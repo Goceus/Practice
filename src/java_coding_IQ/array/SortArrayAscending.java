f0607c000acc2101db829fe0be3893c5a5b170fa
Goce_Cydeo <goce.trpevski@gmail.com>
2023-08-01 11:38:23 -0400

    SortArrayAscending file is created.

---
+++ src/java_coding_IQ/array/SortArrayAscending.java
@@ -0,0 +1,26 @@
+package java_coding_IQ.array;
+
+public class SortArrayAscending {
+
+     /*
+        Sort Array Ascending
+
+        Create a method that will sort the given array in ascending order (smallest to largest).
+
+        DO NOT USE any built in methods
+
+        Ex:
+            Input:
+                [3, 4, 1, 3, 8, 6, -1, 5, 1, 2, 6]
+            Output:
+                [-1, 1, 1, 2, 3, 3, 4, 5, 6, 6, 8]
+
+     */
+
+
+
+    public static void main(String[] args) {
+
+
+    }
+}

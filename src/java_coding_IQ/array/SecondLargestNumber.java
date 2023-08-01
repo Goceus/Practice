3bea730e77835efe8f75a33c075bebd31bb774c2
Goce_Cydeo <goce.trpevski@gmail.com>
2023-08-01 11:21:28 -0400

    SecondLargestNumber file created

---
+++ src/java_coding_IQ/array/SecondLargestNumber.java
@@ -0,0 +1,22 @@
+package java_coding_IQ.array;
+
+public class SecondLargestNumber {
+
+        /*
+        Second Largest Number
+
+        Create a method that finds the second largest number in the given array. The second largest must be different from the largest number.
+
+        Ex:
+            Input:
+                [3, 4, 5, 1, 2, 6]
+            Output:
+                5
+
+     */
+
+    public static void main(String[] args) {
+
+
+    }
+}

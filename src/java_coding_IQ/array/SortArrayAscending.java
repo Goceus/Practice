c025ed6ae42ebc6e2f96116bcd5ac22cc0e2a752
Goce_Cydeo <goce.trpevski@gmail.com>
2023-08-01 11:43:00 -0400

    SortArrayAscending task is finished.

--- src/java_coding_IQ/array/SortArrayAscending.java
+++ src/java_coding_IQ/array/SortArrayAscending.java
@@ -1,5 +1,7 @@
 package java_coding_IQ.array;
 
+import java.util.Arrays;
+
 public class SortArrayAscending {
 
      /*
@@ -18,8 +20,25 @@ public class SortArrayAscending {
      */
 
 
+    public static int[] sortArrInAsc(int[] arr){
+
+        for (int i = 0; i < arr.length; i++) {// sorts an array in ascending order.
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+
+    }
 
     public static void main(String[] args) {
+
+        int[] x = {3, 4, 1, 3, 8, 6, -1, 5, 1, 2, 6};
+        System.out.println(Arrays.toString(sortArrInAsc(x)));
 
 
     }

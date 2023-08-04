32e68c4bf5452f567b3ea8b5ffed4b49f78fa193
Goce_Cydeo <goce.trpevski@gmail.com>
2023-08-03 20:04:01 -0400

    FrequencyOfCharacter class created

---
+++ src/java_coding_IQ/collections/FrequencyOfCharacter.java
@@ -0,0 +1,23 @@
+package java_coding_IQ.collections;
+
+public class FrequencyOfCharacter {
+
+ /*
+        Frequency of Character
+
+        Create a method that will determine the frequency of every character in the given String
+            parameter: String
+            return: void
+
+        Ex:
+            input:
+                frequencyOfCharacter("banana apple pie")
+            output:
+                {b=1, a=4, n=2,  =2, p=3, l=1, e=2, i=1}
+     */
+
+    public static void main(String[] args) {
+
+
+    }
+}
